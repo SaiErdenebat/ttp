c9313475c8b47c2de4cec6685abffaf2c71952ec
Sai <saikhna08@gmail.com>
2024-08-02 11:06:39 -0600

    lots of change

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ try:
 
 
     r = s.get(tradesUrl, headers={'Authorization': "Bearer {}".format(token)})
-    
+
     df = pd.json_normalize(r.json()['data']['results'])
 
     ###
@@ -46,29 +46,44 @@ try:
     df['percent'] = round((df['entry']-df['exit'])/df['entry']*100, 2)##.astype(str) + '%'
     df['closedDateOnly'] = df['closeDate'].str.slice(0, 10)
     #df = df.sort_values(by=['closedDateOnly'])
-    df = df.sort_values(by=['openDate'])
+    df = df.sort_values(by=['closeDate'])
     df['balance'] = df['profitAndLoss'].cumsum()
     df = df.reset_index(drop=True)
     
-    filtered = df[['createdAt', 'closeDate','closedDateOnly', 'symbol', 'quantity', 'entry', 'exit', 'percent', 'profitAndLoss', 'balance', 'cost']]
+    filtered = df[['createdAt', 'closeDate','closedDateOnly', 'symbol', 'quantity', 'entry', 'exit', 'percent', 'profitAndLoss', 'cost', 'balance']]
 
     
     #print(filtered.to_string())
-    #filtered['profitAndLoss'].plot(kind='bar')
-    
-    filtered['balance'].plot.line()
-   # st.bar_chart(filtered['profitAndLoss'])
-   # st.line_chart(filtered['balance'])
 
-   # grouped = filtered.groupby('closedDateOnly').sum().reset_index()
-    #print(grouped[['closedDateOnly', 'profitAndLoss']])
-    #grouped[['closedDateOnly', 'profitAndLoss']].plot(kind='bar')
-   # st.bar_chart(grouped[['profitAndLoss']])
+    st.line_chart(filtered['balance'])
+
+    grouped = filtered.groupby('closedDateOnly').sum().reset_index()
     st.bar_chart(filtered['profitAndLoss'])
     #print(filtered)
     st.bar_chart(
         filtered, x='closeDate', y=['profitAndLoss','cost'], color=["#FF0000", "#0000FF"]  # Optional
     )
     st.bar_chart(filtered['percent'])
+    st.bar_chart(grouped, x='closedDateOnly', y=['profitAndLoss'])
+    #print(filtered.groupby('closedDateOnly')['symbol'].nunique())
+
+
+    grouped_df = filtered.groupby('closedDateOnly')
+    st.scatter_chart(filtered['profitAndLoss'])
+    for key, item in grouped_df:
+        temp = grouped_df.get_group(key)
+        print(temp.to_string(index=False))
+        temp_symbol = temp['symbol']
+        print(temp_symbol.nunique(), temp_symbol.unique())
+        print(round(temp['profitAndLoss'].sum(), 2))
+
+        st.dataframe(temp.iloc[:, 2:], column_config={"percent": st.column_config.NumberColumn(format="%d %%")}, hide_index=True)
+        st.text(temp_symbol.nunique())
+        st.text(temp_symbol.unique())
+        st.text(round(temp['profitAndLoss'].sum(), 2))
+        st.bar_chart(temp, x='symbol', y=['profitAndLoss'])
+    
+    st.dataframe(grouped['profitAndLoss'])
+    
 except KeyError:
     print("keyError") 
